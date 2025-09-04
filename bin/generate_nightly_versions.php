3883edb7b0e0b72877caae3c3136af87f1dcbd3f
yoanm <4410697+yoanm@users.noreply.github.com>
2025-09-04 16:59:56 +0200

    Update generate_nightly_versions.php

--- bin/generate_nightly_versions.php
+++ bin/generate_nightly_versions.php
@@ -2,7 +2,7 @@
 include "../web-php-repo/include/branches.inc";
 include "../web-php-repo/include/release-qa.php";
 
-function _format_version(string $v): string {
+function _format_short_version(string $v): string {
   return preg_replace('/^(\d+\.\d+).*$/', '\1', $v);
 }
 $activeVersionList = array_flip(
@@ -17,14 +17,18 @@ $activeVersionList = array_flip(
 );
 $qaReleaseList = array_filter(
   $QA_RELEASES, 
-  fn (string $key): bool => !array_key_exists(_format_version($key), $activeVersionList),
+  fn (string $key): bool => !array_key_exists(_format_short_version($key), $activeVersionList),
   ARRAY_FILTER_USE_KEY,
 );
 
 $output = [];
 foreach ($qaReleaseList as $version => $data) {
   if (is_array($data) && $data['active'] === true && $data['release']['number'] > 0) {
-    $output[_format_version($version)] = ['version' => $version, ...$data];
+    $rootKey = preg_replace('/^(\d+)\..*$/', '\1', $v);
+    $subKey = _format_short_version($version);
+    $output[$rootKey] ?= []
+    $output[$rootKey][$subKey] ?= []
+    $output[$rootKey][$subKey][$version] = $data;
   }
 }
 ksort($output, SORT_REGULAR);
